03eb555ae72aba0c45b62b1cdfa22d71b0cc043e
TARUC <TARUC@PC-87>
2018-11-02 11:10:11 +0800

    Version 2

--- Desktop/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ Desktop/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,8 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("ABC");
         System.out.println("Hello");
+        System.out.println("123");
     }
 }
